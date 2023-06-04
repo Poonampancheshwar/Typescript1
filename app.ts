b03b9868456ef28c52c573d372fdfecec78bae6e
Poonampancheshwar <Poonampancheshwar1999@gmail.com>
2023-06-04 17:50:15 +0530

    second commit

--- app.ts
+++ app.ts
@@ -2,13 +2,13 @@ console.log('this is my fisrt typescript project');
 
 const fun1 = () => {
 const a:number = 40;
-const b:string = '20';
+const b:number = '20';
 const add = a+b;
 console.log(add);
 };
 fun1();
 
 
-const addFun = (a,b) => a+b;
+const addFun = (a:number,b:number) => a+b;
 const addReturn = addFun(1,2);
 console.log(addReturn);
