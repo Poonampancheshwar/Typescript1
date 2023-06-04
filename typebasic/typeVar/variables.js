4b6e11714f0f1a6f94becce2f32da10a2d5c3d34
Poonampancheshwar <Poonampancheshwar1999@gmail.com>
2023-06-04 23:33:38 +0530

    third commit

---
+++ typebasic/typeVar/variables.js
@@ -0,0 +1,11 @@
+var checkTypes = function () {
+    var status = 'add';
+    status = 'sub';
+    status = 'mul';
+    status = 'status-add';
+    status = 'status-sub';
+    var string1 = 'string1';
+    console.log('status', status, typeof status);
+    console.log('string1', string1, typeof status);
+};
+checkTypes();
